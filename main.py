1d87b5618a0477bb60409e5b61735d3210e9e6d8
Smoothchocolate9 <parveen2sonam@gmail.com>
2026-03-03 18:21:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
- import numpy
+import matplotlib.pyplot as plt 
+time=[0, 1, 2, 3, 4 ,5] 
+velocity=[0, 10, 20, 20, 30, 40]
+plt.plot(time, velocity)
+plt.title("🚲Bicycle speed over time🚲")
+plt.xlabel("Time (hours)")
+plt.ylabel("velocity(km/ph)")
 
-import pandas
-
-import matplotlib
-
-import seaborn
-
-print("Everything works!")
-
+plt.show()
